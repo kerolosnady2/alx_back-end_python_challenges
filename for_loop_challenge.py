3c10512d2777a0209d7ca4833f77bb7363b43224
kerolosnady2 <kerolosnady777@gmail.com>
2025-07-19 18:47:35 +0300

    The Program Is For Adding Numbers And Return Th Total

---
+++ for_loop_challenge.py
@@ -0,0 +1,13 @@
+'''
+This Program Is For Add The Numbers Inside The List and Return The Total.
+It's a Static Program, You Can Change The Numebrs Inside The List Manually.
+'''
+
+numbers = [1, 5, 3, 9]
+
+total = 0
+
+for number in numbers:
+    total += number
+
+print(f"Total of The Number is {total}")
